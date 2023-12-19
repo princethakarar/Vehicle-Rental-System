f1d06a99d52dbc5e62d160cd916295fe249ecbe0
Prince Thakarar <prince.thakarar40@gmail.com>
2023-12-19 18:04:36 +0530

    save changes

---
+++ register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="login&register.css">
+</head>
+<body>
+    <div class="container">
+        <form action="get">
+        <h2 class="heading">Login</h2>
+            <input type="text" placeholder="Enter Email" name="mail">
+            <input type="password" placeholder="Enter Password" name="pass">
+            <input type="submit" name="sb" value="Login" class="btn">
+            <p class="register">Don't have account? <a href="register.php">Register</a></p>
+        </form>
+    </div>
+</body>
+</html>
